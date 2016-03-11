6ed69f34375cf752e340165a0ebb19d1df493678
hectorlf <hectorlopezfernandez@hotmail.com>
2016-03-11 09:46:09 +0100

    Fix a test that wasn't complete

--- src/test/java/com/mitchellbosecke/pebble/boot/test/ControllerTest.java
+++ src/test/java/com/mitchellbosecke/pebble/boot/test/ControllerTest.java
@@ -4,6 +4,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,41 +14,38 @@ import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(Application.class)
-@WebIntegrationTest(randomPort=true)
+@WebIntegrationTest(randomPort = true)
 public class ControllerTest {
 
-	@Autowired
-	private WebApplicationContext wac;
+    @Autowired
+    private WebApplicationContext wac;
 
-	protected MockMvc mockMvc;
+    protected MockMvc mockMvc;
 
-	@Before
-	public void setup() {
-		this.mockMvc = MockMvcBuilders
-			.webAppContextSetup(this.wac)
-			.build();
-	}
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
 
-	@Test
-	public void testOk() throws Exception {
-		mockMvc.perform(get("/index.action"))
-			.andExpect(status().isOk())
-			.andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
-			.andExpect(content().string("Hello Pebbleworld!"));
-	}
+    @Test
+    public void testOk() throws Exception {
+        mockMvc.perform(get("/index.action")).andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(content().string("Hello Pebbleworld!"));
+    }
 
-	@Test
-	public void testRequestAccess() throws Exception {
-		mockMvc.perform(get("/contextPath.action"))
-			.andExpect(status().isOk())
-			.andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
-			.andExpect(content().string("ctx path:"));
-	}
+    @Test
+    public void testRequestAccess() throws Exception {
+        MvcResult result = mockMvc.perform(get("/contextPath.action")).andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML)).andReturn();
+        Assert.assertEquals("ctx path:" + result.getRequest().getContextPath(),
+                result.getResponse().getContentAsString());
+    }
 
 }
