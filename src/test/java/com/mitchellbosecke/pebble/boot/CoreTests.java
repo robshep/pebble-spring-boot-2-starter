f2d6870c0275413409e9f61fe6e2ed88053204e2
hectorlf <hectorlopezfernandez@hotmail.com>
2017-03-17 20:06:02 +0100

    Add a basic test for non-web environments

---
+++ src/test/java/com/mitchellbosecke/pebble/boot/CoreTests.java
@@ -0,0 +1,28 @@
+package com.mitchellbosecke.pebble.boot;
+
+import java.io.StringWriter;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.mitchellbosecke.pebble.PebbleEngine;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = NonWebApplication.class)
+public class CoreTests {
+
+	@Autowired
+	private PebbleEngine pebbleEngine;
+
+    @Test
+    public void testOk() throws Exception {
+    	StringWriter sw = new StringWriter();
+        pebbleEngine.getTemplate("templates/hello.pebble").evaluate(sw);
+        Assert.assertTrue(sw.toString() != null && !sw.toString().isEmpty());
+    }
+
+}
