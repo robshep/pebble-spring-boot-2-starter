7ffbf78aa0d6e1438d1a441dc120c5365273866f
Rob Shepherd <rgshepherd@gmail.com>
2017-10-08 21:35:47 +0100

    support for Spring-Boot milestone release 2.0.0.M4

--- src/test/java/com/mitchellbosecke/pebble/boot/CoreTests.java
+++ src/test/java/com/mitchellbosecke/pebble/boot/CoreTests.java
@@ -6,13 +6,13 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.mitchellbosecke.pebble.PebbleEngine;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = NonWebApplication.class)
+@SpringBootTest(classes = NonWebApplication.class)
 public class CoreTests {
 
     @Autowired
